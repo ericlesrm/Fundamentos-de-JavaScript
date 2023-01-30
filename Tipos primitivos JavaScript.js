85c325153dc0fbc2ff80ebaa827960a195578ac2
Ericles R. Marques <115119008+ericlesrmarques@users.noreply.github.com>
2023-01-30 13:07:42 -0300

    Add files via upload

---
+++ Tipos primitivos JavaScript.js
@@ -0,0 +1,58 @@
+//  TIPO NUMBER //
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+// Ponto flutuante, o que é? O JavaScript representa números usando o formato de ponto flutuante de 64 bits definido pelo padrão IEEE 754,
+// o que significa que pode representar números tão grandes quanto ± 1,7976931348623157 × 10 e tão pequenos quanto ± 5 × 10.
+
+//Exemplo de ponto flutuante:
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// Not-A-Number NaN -> A propriedade global NaN é um valor especial que significa Not-A-Number (não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
+
+
+
+//  TIPO STRING  //
+
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// Concatenação (+), o que é? Concatenar é uma palavra da programação que significa "colocar junto". 
+// Para colocar strings juntas em JavaScript, usamos o operador (+), o mesmo usamos para adicionar números, mas neste contexto é algo diferente.
+
+//Exemplo de concatenação:
+
+console.log(citacao + meuNome)
+
+//template string OU template literal é um recurso da linguagem que permite declarar strings dentro das quais podemos interpolar valores ou ainda quebrar o texto em múltiplas linhas.
+
+
+
+// TIPO BOOLEAN // 
+
+//Tipo boolean  um tipo de dado lógico que pode ter apenas um de dois valores possíveis: verdadeiro ou falso.
+
+
+
